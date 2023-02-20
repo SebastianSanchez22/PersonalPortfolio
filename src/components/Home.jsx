4ca407a6e49a49a7f201d63f4d233990dc84ac08
SebastianSanchez22 <sebastiansaor22@gmail.com>
2023-02-20 06:54:59 -0500

    Changing text size for mobile responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-[#8892b0] text-xl hover:text-green-600' >Hi, my name is</p>
-        <h1 className='animate-typing overflow-hidden whitespace-nowrap animate-text bg-gradient-to-r from-green-700 via-purple-500 to-orange-500 text-4xl sm:text-7xl font-bold bg-clip-text text-transparent'>Sebastian Sanchez</h1>
+        <h1 className='animate-typing overflow-hidden whitespace-nowrap animate-text bg-gradient-to-r from-green-700 via-purple-500 to-orange-500 text-4xl sm:text-6xl font-bold bg-clip-text text-transparent'>Sebastian Sanchez</h1>
         <h2 className='text-2xl sm:text-5xl font-bold text-[#8892b0] hover:text-green-600'>Software Engineering / CS student.</h2>
         <p className='text-[#8892b0] py-4 max-w-[750px] hover:text-green-600'>I'm a 4th year student in EIA University, currently focusing in software development and data science.</p>
         <div>
